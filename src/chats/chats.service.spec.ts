9ba82a0770dbdd4d40b23e3b1ac9fdb8c4726472
soratsm <hiyama.work@gmail.com>
2022-06-18 13:44:25 +0900

    Create Heroku workflows

--- src/chats/chats.service.spec.ts
+++ src/chats/chats.service.spec.ts
@@ -1,58 +1,13 @@
+import { ChatsService } from '@/chats/chats.service';
+import { PrismaService } from '@/prisma.service';
 import { Test, TestingModule } from '@nestjs/testing';
 import { PrismaClient } from '@prisma/client';
-
-import { PrismaService } from '@/prisma.service';
-import { ChatsService } from '@/chats/chats.service';
 
 // データ投入の術を別途検討するよりテストプログラムで代用する
 describe('ChatsService', () => {
   let service: ChatsService;
   const timeout = 1000 * 40;
-  const inputData01 = {
-    id: 'automation_tool',
-    question: '自動化ツール開発についてですね。コチラからお問い合わせできます。',
-    deleted: false,
-    answers: {
-      create: [
-        {
-          order: 1,
-          content: '問い合わせる',
-          nextId: 'contact',
-        },
-        {
-          order: 2,
-          content: '最初の質問に戻る',
-          nextId: 'init',
-        },
-      ],
-    },
-  };
-
-  const inputData02 = {
-    id: 'chatbot',
-    question: 'xxx',
-    deleted: false,
-    answers: {
-      create: [
-        {
-          order: 1,
-          content: '参考',
-          nextId: 'https://www.youtube.com/playlist?list=PLX8Rsrpnn3IVOk48awq_nKW0aFP0MGpnn',
-        },
-        {
-          order: 2,
-          content: 'その他のポートフォリオについて知りたい',
-          nextId: 'portfolio',
-        },
-        {
-          order: 3,
-          content: '最初の質問に戻る',
-          nextId: 'init',
-        },
-      ],
-    },
-  };
-  const inputData03 = {
+  const inputData000 = {
     id: 'init',
     question: 'こんにちは(^^) soratsmへのご用件はなんでしょうか？',
     deleted: false,
@@ -68,10 +23,15 @@ describe('ChatsService', () => {
           content: 'ポートフォリオについて知りたい',
           nextId: 'portfolio',
         },
+        {
+          order: 3,
+          content: 'コンタクトを取りたい',
+          nextId: 'dating',
+        },
       ],
     },
   };
-  const inputData04 = {
+  const inputData100 = {
     id: 'job_offer',
     question: 'どのようなお仕事でしょうか？',
     deleted: false,
@@ -95,7 +55,47 @@ describe('ChatsService', () => {
       ],
     },
   };
-  const inputData05 = {
+  const inputData110 = {
+    id: 'webapp',
+    question:
+      'Webアプリ開発についてですね。Reactによる当サイトのようなWebアプリなどが可能です。詳細はコチラからお問い合わせできます。',
+    deleted: false,
+    answers: {
+      create: [
+        {
+          order: 1,
+          content: '問い合わせる',
+          nextId: 'contact',
+        },
+        {
+          order: 9,
+          content: '最初の質問に戻る',
+          nextId: 'init',
+        },
+      ],
+    },
+  };
+  const inputData120 = {
+    id: 'automation_tool',
+    question:
+      '自動化ツール開発についてですね。VBA|GAS|API|スクレイピングなどが可能です。詳細はコチラからお問い合わせできます。',
+    deleted: false,
+    answers: {
+      create: [
+        {
+          order: 1,
+          content: '問い合わせる',
+          nextId: 'contact',
+        },
+        {
+          order: 9,
+          content: '最初の質問に戻る',
+          nextId: 'init',
+        },
+      ],
+    },
+  };
+  const inputData130 = {
     id: 'other_jobs',
     question: 'その他についてですね。コチラからお問い合わせできます。',
     deleted: false,
@@ -107,69 +107,101 @@ describe('ChatsService', () => {
           nextId: 'contact',
         },
         {
-          order: 2,
+          order: 9,
           content: '最初の質問に戻る',
           nextId: 'init',
         },
       ],
     },
   };
-  const inputData06 = {
-    id: 'other_portfolio',
-    question: 'xxx',
+  const inputData200 = {
+    id: 'portfolio',
+    question:
+      'キーワード『React/TypeScript/MUI/ChakraUI/Express/Firebase/Supabase』あたりで作成しています。詳しくはinfoページまで',
     deleted: false,
     answers: {
       create: [
         {
           order: 1,
+          content: 'ETF一覧について知りたい',
+          nextId: 'etfs',
+        },
+        {
+          order: 2,
+          content: 'チャットボットについて知りたい',
+          nextId: 'chatbot',
+        },
+        {
+          order: 9,
+          content: '最初の質問に戻る',
+          nextId: 'init',
+        },
+      ],
+    },
+  };
+  const inputData210 = {
+    id: 'etfs',
+    question:
+      'ETF一覧及び各ETFのページは静的なサイトを提供しているため、非常に高速に情報を確認できます。',
+    deleted: false,
+    answers: {
+      create: [
+        {
+          order: 1,
+          content: '参考にさせて頂いたモノ',
+          nextId: 'https://nextjs.org/docs/basic-features/pages',
+        },
+        {
+          order: 2,
           content: 'その他のポートフォリオについて知りたい',
           nextId: 'portfolio',
         },
         {
-          order: 2,
+          order: 9,
           content: '最初の質問に戻る',
           nextId: 'init',
         },
       ],
     },
   };
-  const inputData07 = {
-    id: 'portfolio',
-    question: 'react MUI ChakraUI',
+  const inputData220 = {
+    id: 'chatbot',
+    question:
+      'チャットボットは下記を参考に『Next.js|Supabase|Chakra UI|関数Component』と色々置き換えつつ、今見ているボットさんが語りかけています。',
     deleted: false,
     answers: {
       create: [
         {
           order: 1,
-          content: 'チャットボットについて知る',
-          nextId: 'chatbot',
+          content: '参考にさせて頂いたモノ',
+          nextId: 'https://www.youtube.com/playlist?list=PLX8Rsrpnn3IVOk48awq_nKW0aFP0MGpnn',
         },
         {
           order: 2,
-          content: 'xxxについて知りたい',
-          nextId: 'other_portfolio',
+          content: 'その他のポートフォリオについて知りたい',
+          nextId: 'portfolio',
         },
         {
-          order: 3,
+          order: 9,
           content: '最初の質問に戻る',
           nextId: 'init',
         },
       ],
     },
   };
-  const inputData08 = {
-    id: 'webapp',
-    question: 'Webアプリ開発についてですね。コチラからお問い合わせできます。',
+  const inputData300 = {
+    id: 'dating',
+    question: 'まずは一緒にランチでもいかがですか？DMしてください。',
     deleted: false,
     answers: {
       create: [
         {
           order: 1,
-          content: '問い合わせる',
-          nextId: 'contact',
+          content: 'DMする',
+          nextId: 'https://twitter.com/soratsm',
         },
         {
-          order: 2,
+          order: 9,
           content: '最初の質問に戻る',
           nextId: 'init',
         },
@@ -201,14 +233,15 @@ describe('ChatsService', () => {
     it(
       '[Create]DBの動作確認用に作ったテスト（確認が済んだら "xit" でテストが実行されないようにする）',
       async () => {
-        await service.createAllRelatedData(inputData01);
-        await service.createAllRelatedData(inputData02);
-        await service.createAllRelatedData(inputData03);
-        await service.createAllRelatedData(inputData04);
-        await service.createAllRelatedData(inputData05);
-        await service.createAllRelatedData(inputData06);
-        await service.createAllRelatedData(inputData07);
-        await service.createAllRelatedData(inputData08);
+        await service.createAllRelatedData(inputData000);
+        await service.createAllRelatedData(inputData100);
+        await service.createAllRelatedData(inputData110);
+        await service.createAllRelatedData(inputData120);
+        await service.createAllRelatedData(inputData130);
+        await service.createAllRelatedData(inputData200);
+        await service.createAllRelatedData(inputData210);
+        await service.createAllRelatedData(inputData220);
+        await service.createAllRelatedData(inputData300);
       },
       timeout,
     );
--- src/symbols/schedule/schedule.service.ts
+++ src/symbols/schedule/schedule.service.ts
@@ -3,16 +3,17 @@ import { Cron } from '@nestjs/schedule';
 
 import { DomainService } from '@/symbols/domain/domain.service';
 
+/**
+ * 定期実行
+ * {@link https://crontab.guru/examples.html CronExamples}
+ */
+
 @Injectable()
 export class ScheduleService {
   private readonly logger = new Logger(ScheduleService.name);
   constructor(private readonly domainService: DomainService) {}
 
-  // 間隔指定'*/30 * * * * *'で30秒ごと
-  // 間隔指定'*/10 * * * *'で10分ごと
-  // 間隔指定'*/5 * * * *'で5分ごと
-  // 間隔指定'0 * * * *'で毎時0分に実行
-  @Cron('*/10 * * * *')
+  @Cron('*/30 * * * *')
   async scheduleForDomainOfFetchAndSaveSymbolData() {
     this.logger.debug('Started "fetchAndSaveSymbolData"');
     await this.domainService.fetchAndSaveSymbolData();
